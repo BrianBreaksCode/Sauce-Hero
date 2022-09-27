8ec9dba6be0798a86b17ec1ac10c028074cb57ad
Mayori <BaconBreak@protonmail.com>
2022-09-28 00:16:06 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
 import discord
 import constants
 
-bot = discord.Bot()
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
 
 @bot.event
 async def on_ready():
@@ -10,5 +11,10 @@ async def on_ready():
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
+
+@bot.event
+async def on_message(message):
+    if "twitter" in message.content:
+        await message.reply("Twitter!!")
 
 bot.run(constants.bot_token) # run the bot with the token
